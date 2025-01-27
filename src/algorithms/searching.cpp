e63d9a1f5838dfda56347b5c5ff57a3c10e9e798
Anirudh Konidala <kidskodingclub@gmail.com>
2025-01-27 03:14:27 -0600

    Added support for gtest suite + binary search

--- src/algorithms/searching.cpp
+++ src/algorithms/searching.cpp
@@ -1,9 +1,25 @@
 #include "algorithms/searching.h"
 
-int linearSearch(int array[], int target) {
-    for(int i = 0; i < sizeof(array); i++) {
+int linearSearch(int array[], int size, int target) {
+    for(int i = 0; i < size; i++) {
         if(array[i] == target) {
             return i;  
+        }
+    }
+    return -1;
+}
+
+int binarySearch(int array[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if(target < array[mid]) {
+            right = mid - 1;
+        } else if(target > array[mid]) {
+            left = mid + 1;
+        } else {
+            return mid;
         }
     }
     return -1;
--- tests/searching_test.cpp
+++ tests/searching_test.cpp
@@ -1,10 +1,19 @@
 #include <gtest/gtest.h>
 #include "../include/algorithms/searching.h"
 
-TEST(SearchingTest, LinearSearchTest) {
+TEST(all_tests, LinearSearchTest) {
     int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int result = linearSearch(data, 10);
-    int result_exist = linearSearch(data, 1);
+    int result = linearSearch(data, std::size(data), 10);
+    int result_exist = linearSearch(data, std::size(data), 1);
+
+    EXPECT_EQ(result, -1);
+    EXPECT_EQ(result_exist, 0);
+}
+
+TEST(all_tests, BinarySearchTest) {
+    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int result = binarySearch(data, std::size(data), 10);
+    int result_exist = binarySearch(data, std::size(data), 1);
 
     EXPECT_EQ(result, -1);
     EXPECT_EQ(result_exist, 0);
