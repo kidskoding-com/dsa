829b57e5faf1c28b0e19556d43455499870c440c
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-01 13:26:42 -0600

    added stack implementation

---
+++ include/data_structures/stack.h
@@ -0,0 +1,46 @@
+#ifndef STACK_H
+#define STACK_H
+#include <stdexcept>
+
+#include "node.h"
+
+template <typename T>
+class Stack {
+public:
+	T* data;
+	unsigned int top;
+	unsigned int capacity;
+
+	Stack() : top(-1), capacity(100) { this->data = new T[this->capacity]; }
+	~Stack() { delete[] this->data; }
+
+	void push(T value) {
+		if(this->top + 1 >= this->capacity) {
+			throw std::runtime_error("Stack overflow");
+		}
+		this->data[++this->top] = value;
+	}
+
+	T peek() {
+		if(isEmpty()) {
+			throw std::runtime_error("No such element");
+		}
+		return this->data[this->top];
+	}
+	
+	T pop() {
+		if(isEmpty()) {
+			throw std::runtime_error("No such element");
+		}
+		return this->data[this->top--];
+	}
+
+	bool isEmpty() {
+		return this->top == -1;
+	}
+	unsigned int getSize() {
+		return this->top + 1;
+	}
+};
+
+#endif //STACK_H
---
+++ tests/stack_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include "data_structures/stack.h"
+
+class StackTest : public testing::Test {
+protected:
+    Stack<int> stack;
+};
+
+TEST_F(StackTest, PushAndGetSize) {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    EXPECT_EQ(stack.getSize(), 3);
+}
+
+TEST_F(StackTest, Pop) {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    EXPECT_EQ(stack.pop(), 3);
+    EXPECT_EQ(stack.pop(), 2);
+    EXPECT_EQ(stack.pop(), 1);
+}
+
+TEST_F(StackTest, Peek) {
+    stack.push(1);
+    stack.push(2);
+    EXPECT_EQ(stack.peek(), 2);
+    stack.pop();
+    EXPECT_EQ(stack.peek(), 1);
+}
+
+TEST_F(StackTest, IsEmpty) {
+    EXPECT_TRUE(stack.isEmpty());
+    stack.push(1);
+    EXPECT_FALSE(stack.isEmpty());
+    stack.pop();
+    EXPECT_TRUE(stack.isEmpty());
+}
+
+TEST_F(StackTest, StackOverflow) {
+    for (int i = 0; i < 100; ++i) {
+        stack.push(i);
+    }
+    EXPECT_THROW(stack.push(101), std::runtime_error);
+}
